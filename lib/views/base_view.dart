f318942f14276a97295ae7d0c5bc4be3b706d7fb
aytug.sevgi <aytug3667@gmail.com>
2021-02-03 16:31:03 +0300

    dynamic base view

--- lib/views/base_view.dart
+++ lib/views/base_view.dart
@@ -5,6 +5,7 @@ import 'package:examapp/views/about_view.dart';
 import 'package:examapp/views/classroom_view.dart';
 import 'package:examapp/views/contact_view.dart';
 import 'package:examapp/views/home_view.dart';
+import 'package:examapp/widget/responsive_widget.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,22 +17,32 @@ class BaseView extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            top: 100,
-            right: -context.dynamicWidth(0.08),
-            child: Image.asset(
-              "assets/background.jpg",
-              fit: BoxFit.contain,
-              width: context.dynamicWidth(0.72),
+          AnimatedPositioned(
+            top: ResponsiveWidget.isSmallScreen(context) ? 40 : 100,
+            right: ResponsiveWidget.isSmallScreen(context) ? 10 : -50,
+            duration: Duration(seconds: 1),
+            child: AnimatedContainer(
+              duration: Duration(seconds: 1),
+              height: ResponsiveWidget.isSmallScreen(context)
+                  ? 100
+                  : ResponsiveWidget.isMediumScreen(context)
+                      ? 500
+                      : 600,
+              child: Image.asset(
+                "assets/background.jpg",
+                fit: BoxFit.contain,
+              ),
             ),
           ),
           Align(
             alignment: Alignment.topCenter,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                appBar(context),
-                Expanded(child: body(context)),
+                buildAppBar(context),
+                SizedBox(
+                  height: 50,
+                ),
+                Expanded(child: ListView(children: [body(context)])),
               ],
             ),
           ),
@@ -40,65 +51,107 @@ class BaseView extends StatelessWidget {
     );
   }
 
-  SizedBox appBar(BuildContext context) {
+  Widget buildAppBar(BuildContext context) {
+    return ResponsiveWidget(
+        largeScreen: _appBarLarge(context),
+        mediumScreen: _appBarSmall(context),
+        smallScreen: _appBarSmall(context));
+  }
+
+  SizedBox _appBarLarge(BuildContext context) {
     return SizedBox(
-      height: 150,
+      height: 100,
       width: context.dynamicWidth(1),
-      child: Center(
-        child: Row(
-          children: [
-            Spacer(flex: 2),
-            SizedBox(
-                width: 130,
-                child: FlatButton(
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, "/");
-                  },
-                  child: RichText(
-                    maxLines: 1,
-                    text: TextSpan(
-                        style: context.themeData.textTheme.display1,
-                        children: [
-                          TextSpan(text: "E"),
-                          TextSpan(text: "G"),
-                          TextSpan(text: "E"),
-                          TextSpan(
-                            text: "xam",
-                            style:
-                                context.themeData.textTheme.display1.copyWith(
-                              color: Color(0xFFFD7272),
-                            ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Spacer(flex: 2),
+          SizedBox(
+              width: 130,
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, "/");
+                },
+                child: RichText(
+                  maxLines: 1,
+                  text: TextSpan(
+                      style: context.themeData.textTheme.display1,
+                      children: [
+                        TextSpan(text: "E"),
+                        TextSpan(text: "G"),
+                        TextSpan(text: "E"),
+                        TextSpan(
+                          text: "xam",
+                          style: context.themeData.textTheme.display1.copyWith(
+                            color: Color(0xFFFD7272),
                           ),
-                        ]),
-                  ),
-                )),
-            Spacer(flex: 22),
-            tabButton(
-              context: context,
-              tabType: TabType.Home,
-              text: "Home",
-            ),
-            Spacer(flex: 1),
-            tabButton(
-              context: context,
-              tabType: TabType.Classroom,
-              text: "Classroom",
-            ),
-            Spacer(flex: 1),
-            tabButton(
-              context: context,
-              tabType: TabType.Contact,
-              text: "Contact",
-            ),
-            Spacer(flex: 1),
-            tabButton(
-              context: context,
-              tabType: TabType.About,
-              text: "About",
-            ),
-            Spacer(flex: 2),
-          ],
-        ),
+                        ),
+                      ]),
+                ),
+              )),
+          Spacer(flex: 22),
+          tabButton(
+            context: context,
+            tabType: TabType.Home,
+            text: "Home",
+          ),
+          Spacer(flex: 1),
+          tabButton(
+            context: context,
+            tabType: TabType.Classroom,
+            text: "Classroom",
+          ),
+          Spacer(flex: 1),
+          tabButton(
+            context: context,
+            tabType: TabType.Contact,
+            text: "Contact",
+          ),
+          Spacer(flex: 1),
+          tabButton(
+            context: context,
+            tabType: TabType.About,
+            text: "About",
+          ),
+          Spacer(flex: 2),
+        ],
+      ),
+    );
+  }
+
+  SizedBox _appBarSmall(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      width: context.dynamicWidth(1),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Spacer(flex: 2),
+          SizedBox(
+              width: 130,
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, "/");
+                },
+                child: RichText(
+                  maxLines: 1,
+                  text: TextSpan(
+                      style: context.themeData.textTheme.display1,
+                      children: [
+                        TextSpan(text: "E"),
+                        TextSpan(text: "G"),
+                        TextSpan(text: "E"),
+                        TextSpan(
+                          text: "xam",
+                          style: context.themeData.textTheme.display1.copyWith(
+                            color: Color(0xFFFD7272),
+                          ),
+                        ),
+                      ]),
+                ),
+              )),
+          Spacer(flex: 27),
+        ],
       ),
     );
   }
@@ -117,12 +170,14 @@ class BaseView extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(18.0),
         ),
-        child: Text(text,
-            style: currentType == tabType
-                ? context.themeData.textTheme.display3.copyWith(
-                    color: Colors.white,
-                  )
-                : context.themeData.textTheme.display3));
+        child: FittedBox(
+          child: Text(text,
+              style: currentType == tabType
+                  ? context.themeData.textTheme.display3.copyWith(
+                      color: Colors.white,
+                    )
+                  : context.themeData.textTheme.display3),
+        ));
   }
 
   Widget body(BuildContext context) {
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -3,7 +3,12 @@ import 'package:examapp/utils/extension.dart';
 import 'package:examapp/widget/submit_button.dart';
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -12,198 +17,7 @@ class HomeView extends StatelessWidget {
           flex: 2,
         ),
         Expanded(
-          flex: 5,
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            height: 600,
-            child: Form(
-              /*key:
-                  Provider.of<SignUpController>(context, listen: false).formKey,*/
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Spacer(
-                    flex: 8,
-                  ),
-                  Expanded(
-                    flex: 6,
-                    child: FittedBox(
-                      child: Text(
-                        "Register",
-                        style: context.themeData.textTheme.title.copyWith(
-                            fontWeight: FontWeight.bold, fontSize: 25),
-                        textAlign: TextAlign.left,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 6,
-                    child: Divider(
-                      thickness: 4,
-                      color: context.themeData.buttonColor,
-                      endIndent: 280,
-                    ),
-                  ),
-                  Expanded(
-                    flex: 11,
-                    child: RichText(
-                      text: TextSpan(
-                          style: context.themeData.textTheme.subtitle1.copyWith(
-                              fontSize: 14,
-                              color: Colors.black.withOpacity(0.7)),
-                          text:
-                              "Please register to you account to contunie with Ege Examination System."),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 12,
-                    child: TextFormField(
-                      decoration: new InputDecoration(
-                          hintText: "First Name",
-                          errorStyle: context
-                              .themeData.inputDecorationTheme.errorStyle
-                              .copyWith(color: Colors.red.withOpacity(0.8))),
-                      /*validator: (value) => Provider.of<SignUpController>(
-                                context,
-                                listen: false)
-                            .firstNameValidator(),
-                        onChanged: (String value) {
-                          Provider.of<SignUpController>(context, listen: false)
-                              .firstName = value;
-                        }*/
-                    ),
-                  ),
-                  Expanded(
-                    flex: 12,
-                    child: TextFormField(
-                      decoration: new InputDecoration(
-                          hintText: "Last Name",
-                          errorStyle: context
-                              .themeData.inputDecorationTheme.errorStyle
-                              .copyWith(color: Colors.red.withOpacity(0.8))),
-                      /*validator: (value) => Provider.of<SignUpController>(
-                                context,
-                                listen: false)
-                            .lastNameValidator(),
-                        onChanged: (String value) {
-                          Provider.of<SignUpController>(context, listen: false)
-                              .lastName = value;
-                        }*/
-                    ),
-                  ),
-                  Expanded(
-                    flex: 12,
-                    child: TextFormField(
-                      decoration: new InputDecoration(
-                          hintText: "Email Address",
-                          errorStyle: context
-                              .themeData.inputDecorationTheme.errorStyle
-                              .copyWith(color: Colors.red.withOpacity(0.8))),
-                      /*validator: (value) => Provider.of<SignUpController>(
-                                context,
-                                listen: false)
-                            .emailValidator(),
-                        onChanged: (String value) {
-                          Provider.of<SignUpController>(context, listen: false)
-                              .email = value;
-                        }*/
-                    ),
-                  ),
-                  Expanded(
-                    flex: 12,
-                    child: TextFormField(
-                      decoration: new InputDecoration(
-                          hintText: "Password",
-                          errorStyle: context
-                              .themeData.inputDecorationTheme.errorStyle
-                              .copyWith(color: Colors.red.withOpacity(0.8))),
-                      /*validator: (value) => Provider.of<SignUpController>(
-                                context,
-                                listen: false)
-                            .passwordValidator(),
-                        onChanged: (String value) {
-                          Provider.of<SignUpController>(context, listen: false)
-                              .password = value;
-                        }*/
-                    ),
-                  ),
-                  Expanded(
-                      flex: 12,
-                      child: TextFormField(
-                        decoration: new InputDecoration(
-                            hintText: "Confirm Password",
-                            errorStyle: context
-                                .themeData.inputDecorationTheme.errorStyle
-                                .copyWith(color: Colors.red.withOpacity(0.8))),
-                        /*validator: (value) => Provider.of<SignUpController>(
-                                  context,
-                                  listen: false)
-                              .confirmPasswordValidator(),
-                          onChanged: (String value) {
-                            Provider.of<SignUpController>(context,
-                                    listen: false)
-                                .confirmPassword = value;
-                          }*/
-                      )),
-                  Expanded(
-                    flex: 14,
-                    child: Center(
-                        child: SubmitButton(
-                            colors: [
-                          context.themeData.buttonColor,
-                          context.themeData.buttonColor
-                        ],
-                            child: Text(
-                              "Sign Up",
-                              textAlign: TextAlign.center,
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            ),
-                            onTap:
-                                () {} /*async {
-                          await onTapSignUp(context);
-                        } /*_submitButtonOnTap(context)*/,*/
-                            )),
-                  ),
-                  Expanded(
-                    flex: 10,
-                    child: Center(
-                      child: InkWell(
-                        splashColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        onTap: () => Navigator.of(context).pop(),
-                        child: RichText(
-                          textAlign: TextAlign.center,
-                          text: TextSpan(
-                            children: [
-                              TextSpan(
-                                text: "Already have an account\n",
-                                style: context.themeData.textTheme.bodyText1
-                                    .copyWith(
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              TextSpan(
-                                text: "LOGIN",
-                                style: context.themeData.textTheme.bodyText1
-                                    .copyWith(
-                                        fontWeight: FontWeight.bold,
-                                        color: context.themeData.buttonColor),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Spacer(flex: 2),
-                ],
-              ),
-            ),
-          ),
-        ),
-        /*Expanded(
-          flex: 10,
+          flex: 50,
           child: Container(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             height: 600,
@@ -211,10 +25,11 @@ class HomeView extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Expanded(
-                  flex: 20,
+                  flex: 25,
                   child: FittedBox(
                     child: Text(
                       "Welcome!\nEge Examination System",
+                      maxLines: 2,
                       style: context.themeData.textTheme.display3
                           .copyWith(fontWeight: FontWeight.w700, fontSize: 45),
                     ),
@@ -253,22 +68,24 @@ class HomeView extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
                       Expanded(
-                        flex: 42,
+                        flex: 20,
                         child: RaisedButton(
                           color: context.themeData.primaryColor,
                           onPressed: () {},
-                          child: Text(
-                            "GET STARTED",
-                            style:
-                                context.themeData.textTheme.display1.copyWith(
-                              color: Colors.white,
+                          child: FittedBox(
+                            child: Text(
+                              "GET STARTED",
+                              style:
+                                  context.themeData.textTheme.display1.copyWith(
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                      Spacer(flex: 6),
+                      Spacer(flex: 2),
                       Expanded(
-                        flex: 42,
+                        flex: 20,
                         child: OutlinedButton(
                           style: ButtonStyle(
                               side: MaterialStateProperty.all(BorderSide(
@@ -276,16 +93,18 @@ class HomeView extends StatelessWidget {
                             width: 4,
                           ))),
                           onPressed: () {},
-                          child: Text(
-                            "Learn More",
-                            style:
-                                context.themeData.textTheme.display1.copyWith(
-                              color: context.themeData.accentColor,
+                          child: FittedBox(
+                            child: Text(
+                              "Learn More",
+                              style:
+                                  context.themeData.textTheme.display1.copyWith(
+                                color: context.themeData.accentColor,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                      Spacer(flex: 10),
+                      Spacer(flex: 30),
                     ],
                   ),
                 ),
@@ -295,7 +114,7 @@ class HomeView extends StatelessWidget {
               ],
             ),
           ),
-        ),*/
+        ),
         Spacer(
           flex: 10,
         ),
